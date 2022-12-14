14f812ec9ed56c56dece039ab52a4dea0b582c43
lucieu <lucieu@bam.tech>
2022-12-14 15:17:54 +0100

    configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './modules/user/User.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokemon API')
+    .setDescription('Pokemon API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
