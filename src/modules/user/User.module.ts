18c0470abb3c901d32b9a4b4a0de0846f0be112f
lucieu <lucieu@bam.tech>
2023-02-09 09:44:29 +0100

    create user repository

--- src/modules/user/User.module.ts
+++ src/modules/user/User.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '@src/modules/user/infra/User.repository';
 import { UserDAO } from '@src/modules/user/infra/User/User.dao';
 import { UserController } from './controller/User.controller';
 import { UserService } from './domain/User.service';
@@ -7,6 +8,6 @@ import { UserService } from './domain/User.service';
 @Module({
   imports: [TypeOrmModule.forFeature([UserDAO])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
