b6b7a003f0dd2a2359f7ff8be925dde3ef2d44ac
lucieu <lucieu@bam.tech>
2023-02-09 17:50:46 +0100

    create player output dto

--- src/modules/player/controller/Player.controller.ts
+++ src/modules/player/controller/Player.controller.ts
@@ -15,7 +15,7 @@ import {
 } from '@nestjs/swagger';
 import { CreatePlayerInputDTO } from '@src/modules/player/controller/inputTypes/CreatePlayerInput.dto';
 import { GetPlayerInputDTO } from '@src/modules/player/controller/inputTypes/GetPlayerInput.dto';
-import { PlayerEntity } from '@src/modules/player/domain/Player.entity';
+import { PlayerOutputDTO } from '@src/modules/player/controller/outputTypes/PlayerOutput.dto';
 import { PlayerService } from '@src/modules/player/domain/Player.service';
 
 @ApiTags('player')
@@ -27,17 +27,26 @@ export class PlayerController {
   @ApiResponse({
     status: 201,
     description: 'Your player has been successfully saved.',
+    type: PlayerOutputDTO,
   })
   @ApiInternalServerErrorResponse({
     description: 'There is an internal server error.',
   })
   @ApiBody({ type: CreatePlayerInputDTO })
-  create(@Body() { pseudo }: CreatePlayerInputDTO): Promise<PlayerEntity> {
+  create(@Body() { pseudo }: CreatePlayerInputDTO): Promise<PlayerOutputDTO> {
     return this.playerService.createPlayer(pseudo);
   }
 
   @Get()
-  async get(@Query() { id }: GetPlayerInputDTO): Promise<PlayerEntity> {
+  @ApiResponse({
+    status: 200,
+    description: 'Your player has been successfully retrieved.',
+    type: PlayerOutputDTO,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'There is an internal server error.',
+  })
+  async get(@Query() { id }: GetPlayerInputDTO): Promise<PlayerOutputDTO> {
     const player =
       id === undefined ? null : await this.playerService.getPlayer(id);
     if (player === null) {
